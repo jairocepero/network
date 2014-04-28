8c0806c3af0617d8177d41ef7e979cc4ef03e085
jairocepero <Jairo@10.0.1.11>
2014-04-27 22:54:49 -0500

    contains finished and added some new data members

--- network/src/network/NetworkCalc.java
+++ network/src/network/NetworkCalc.java
@@ -1,20 +1,25 @@
 package network;
 
+import java.io.File;
+
 public class NetworkCalc
 {
-	//private static String subnet = "200.34.55.0/24";
-	//private String adr = "";
-	//private String address = "";	
-	public static String [] AS1;
-	public static String [] AS2;
-	public static String [] AS3;
-	public static String [] AS4;
-	public static String [] AS5;
-	public static String [] AS6;
-	public static String [] AS7;
+	//arrays will serve as storage for the ip range for each AS
+	//int as# will serve as storage for the AS number
+	//in cost# will serve as storage for the cost of each provided CDIR
+	public static String [] AS1; public static int as1; public static int cost1;
+	public static String [] AS2; public static int as2; public static int cost2;
+	public static String [] AS3; public static int as3; public static int cost3;
+	public static String [] AS4; public static int as4; public static int cost4;
+	public static String [] AS5; public static int as5; public static int cost5;
+	public static String [] AS6; public static int as6; public static int cost6;
+	public static String [] AS7; public static int as7; public static int cost7;
 	
+	//main
 	public static void main (String args[])
 	{
+		
+		File inputFile = new File ("./addressinputfile1FIX.txt");
 		
 		ASNum("200.34.55.0/24", 1);
 		for (int i = 0; i < AS1.length; i++)
@@ -23,15 +28,15 @@ public class NetworkCalc
 		}
 		
 		
-	}
+	}//end of main
+	
 		//receives a CIDR(subnet) address and AS number to create an array with AS number
 		//the AS number comes from the file as well as the CDIR(subnet)
-		public static void ASNum(String subnet, int ASNumber)
+		public static void ASNum(String cidr, int ASNumber)
 		{
 			
-			SubnetUtils utils = new SubnetUtils(subnet);
+			SubnetUtils utils = new SubnetUtils(cidr);
 			String[] addresses = utils.getInfo().getAllAddresses();
-			
 			
 			if(ASNumber == 1)
 			{
@@ -68,10 +73,19 @@ public class NetworkCalc
 		}//end ASNum
 		
 		
-		//checks if the given ip
+		//checks if the given ip is in the array containing a ip range
 		public boolean containsIp(String[] AS, String address)
 		{
-			
-			return true;
+			boolean contains = false;
+			String addressToCheck = address;
+			for(String s  : AS)
+			{
+				if(s.compareToIgnoreCase(addressToCheck) == 0)
+				{
+					contains = true;
+					return contains;
+				}	
+			}
+			return contains;
 		}	
 }
